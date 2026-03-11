8e3bb2cc4d68b21985322a5708fdc41851649777
Alexandru Pasat <palexandru8@icloud.com>
2026-03-11 14:41:35 +0200

    Move subnets from local zone to standard availability zone

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,28 @@ output "selected_region" {
   description = "AWS region selected for this Terraform project"
   value       = var.aws_region
 }
+
+output "availability_zone_used" {
+  description = "Availability Zone used for the subnets"
+  value       = data.aws_availability_zones.available.names[0]
+}
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  description = "ID of the private subnet"
+  value       = aws_subnet.private.id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the internet gateway"
+  value       = aws_internet_gateway.igw.id
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,27 @@ variable "aws_region" {
   type        = string
   default     = "eu-central-1"
 }
+
+variable "project_name" {
+  description = "Project name used in tags and resource naming"
+  type        = string
+  default     = "terraform-aws-infrastructure"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.10.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.10.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.10.2.0/24"
+}
